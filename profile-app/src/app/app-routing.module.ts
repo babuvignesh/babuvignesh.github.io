82cf5e4aa51c9cfea8a8c2be7e3d4485a7b19fad
Babu Vignesh <babu.vignesh@clarivate.com>
2020-02-29 14:40:18 +0530

    Changeds

--- profile-app/src/app/app-routing.module.ts
+++ profile-app/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContinentsComponent } from './continents/continents.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'continents',
+    component: ContinentsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
